30c1cb79c9b71b8bd980f52bfd0157cd1dcb3c93
akanksha0411 <akankshasukhramani@gmail.com>
2026-08-17 15:03:02 +0530

    feat: add invoice generator with CGST/SGST/IGST logic

--- src/generate_ground_truth.py
+++ src/generate_ground_truth.py
@@ -4,11 +4,13 @@ import random
 import string
 from datetime import date, timedelta
 
-random.seed(69)
-np.random.seed(69)
+# random.seed(69)
+# np.random.seed(69)
 
 N_VENDORS = 20
 STATE_CODES = ["07", "27", "29", "33", "06", "24", "09", "19"]
+fy_start = date(2025, 4, 1)
+fy_end = date(2026, 3, 31)
 
 def make_gstin(state_code: str) -> str:
     "Builds a usable GSTIN code, using the state_code(2) + PAN number (10) + entity(1) + taxpayer-type(Z) + checksum(1)"
@@ -48,5 +50,52 @@ def create_vendors(n_vendors: int) -> pd.DataFrame:
     df = pd.DataFrame(vendors)
     return df
 
+def random_invoice_dates(fy_start: date, fy_end: date) -> date:
+    days_ = (fy_end - fy_start).days
+    return fy_start + timedelta(days = random.randint(0, days_))
+
+
+def create_invoices(vendors: pd.Series, low, high) -> pd.DataFrame:
+    invoices = []
+    invoice_cnt = 1
+    buyer_state_code = "07"
+    for i in vendors:
+        vendor_gstin = i
+        for j in range(random.randint(low, high)):
+            invoice_id = f"INV{invoice_cnt:05d}"
+            invoice_date = random_invoice_dates(fy_start, fy_end)
+            filing_period = f"{invoice_date.month:02d}-{invoice_date.year}"
+            invoice_number = f"INV/{invoice_date.month:02d}{invoice_date.year}/{j+1:05d}"
+            amt_before_tax = round(random.uniform(2000, 250000), 2)
+            gst_rate = random.choice([0.05, 0.12, 0.18, 0.28])
+            state_code = vendor_gstin[:2]
+            if state_code == buyer_state_code:
+                cgst = round(amt_before_tax * gst_rate / 2, 2)
+                sgst = round(amt_before_tax * gst_rate / 2, 2)
+                igst = 0.0
+            else:
+                cgst = 0.0
+                sgst = 0.0
+                igst = round(amt_before_tax * gst_rate, 2)
+            total_tax = round(cgst + sgst + igst, 2)
+            total_invoice_value = round(amt_before_tax + total_tax, 2)
+            invoices.append({
+                "invoice_id" : invoice_id,
+                "vendor_gstin" : vendor_gstin,
+                "invoice_number" : invoice_number,
+                "invoice_date" : invoice_date.isoformat(),
+                "filing_period" : filing_period,
+                "taxable_value" : amt_before_tax,
+                "cgst" : cgst,
+                "sgst" : sgst,
+                "igst" : igst,
+                "total_tax" : total_tax,
+                "total_invoice_value" : total_invoice_value
+            })
+            invoice_cnt += 1
+    df = pd.DataFrame(invoices)
+    return df
+
 if __name__ == "__main__":
-    print(create_vendors(N_VENDORS))
+    vendors = create_vendors(N_VENDORS)
+    print(create_invoices(vendors["vendor_gstin"], 40, 60))
